52005e3f2560d81afb4e180cac135b3f060a8aaa
Ayush Chaudhary <acayush2002@gmail.com>
2022-09-20 04:09:58 +0530

    home completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,14 +4,14 @@ const Home = () => {
   return (
     <div className='homePageWrapper'>
       <div className='formWrapper'>
-        <img src="/code-sync.png" alt="site logo"/>
-        <h4 className='mainLabel'>Insert the Room Code</h4>
+        <img className='homePageLogo' src="/code-sync.png" alt="site logo"/>
+        <h4 className='mainLabel'>Join a room</h4>
         <div className="inputGroup">
           <input type="text" className='inputBox' placeholder='Room Code' />
           <input type="text" className='inputBox' placeholder='Username' />
 
           <button className='btn joinBtn'>Join</button>
-          <span className='createInfo'>Don't have a room code? 
+          <span className='createInfo'>Don't have a room code? &nbsp;
           <a href="sdf" className='createNewBtn'>Make a new room.</a>
           </span>
         </div>
