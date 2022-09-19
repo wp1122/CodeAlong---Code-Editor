250539deee2375a8b33d552a74f45764df4885d5
Ayush Chaudhary <acayush2002@gmail.com>
2022-09-20 04:32:22 +0530

    added toaster and nav to editor

--- src/App.js
+++ src/App.js
@@ -2,11 +2,24 @@ import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import './App.css';
 import Home from "./pages/Home";
 import EditorPage from "./pages/EditorPage";
+import { Toaster } from 'react-hot-toast';
 
 
 function App() {
   return (
     <>
+      <div>
+        <Toaster
+          position = "top-right"
+          toastOptions={{
+            success:{
+              theme:{
+                primary: '4aed88',
+              }
+            }
+          }}
+          />
+      </div>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home/>}></Route>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,41 @@
 import React, { useState } from 'react'
 import {v4 as uuid} from "uuid";
+import toast from "react-hot-toast";
+import { useNavigate } from 'react-router-dom';
+
 const Home = () => {
 
+  const navigate= useNavigate();
   const [roomId, setRoomId] = useState("");
   const [username, setUsername] = useState("");
   const createNewRoom = (e) => {
     e.preventDefault();//prevent reload
     const id= uuid();
     setRoomId(id);
+    toast.success("Created a new room");
+  }
+
+  const joinRoom = () =>{
+    if(!roomId){
+      toast.error("Enter a valid room code");
+      return;
+    }
+    if(!username){
+      toast.error("Enter a valid username");
+      return;
+    }
+    
+    navigate(`/editor/${roomId}`,{
+      state:{
+        username,
+      }
+    })
+  }
+
+  const handleInputEnter = (e) => {
+    if(e.code==="Enter"){
+      joinRoom();
+    }
   }
 
   return (
@@ -15,14 +43,21 @@ const Home = () => {
       <div className='formWrapper'>
         <img className='homePageLogo' src="/code-sync.png" alt="site logo"/>
         <h4 className='mainLabel'>Join a room</h4>
-        <div className="inputGroup">
-          <input type="text" className='inputBox' placeholder='Room Code' value={roomId} onChange={(e)=>setRoomId(e.target.value)} />
-          <input type="text" className='inputBox' placeholder='Username' value={username} onChange={(e)=>setUsername(e.target.value)}/>
 
-          <button className='btn joinBtn'>Join</button>
+        <div className="inputGroup">
+          <input type="text" className='inputBox' placeholder='Room Code' value={roomId} onChange={(e)=>setRoomId(e.target.value)} 
+          onKeyUp={handleInputEnter}
+          />
+
+          <input type="text" className='inputBox' placeholder='Username' value={username} onChange={(e)=>setUsername(e.target.value)}
+          onKeyUp={handleInputEnter}/>
+
+          <button className='btn joinBtn' onClick={joinRoom}>Join</button>
+
           <span className='createInfo'>Don't have a room code? &nbsp;
           <a onClick={createNewRoom} href="sdf" className='createNewBtn'>Make a new room.</a>
           </span>
+
         </div>
       </div>
       <footer>
